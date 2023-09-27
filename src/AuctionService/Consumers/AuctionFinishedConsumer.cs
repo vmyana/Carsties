a41b35289ec59659935610f5caf9af1e2094456a
Vamshi Krishna Myana <krikrishna@deloitte.com>
2023-09-27 12:50:36 +0530

    End of section 6

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,35 @@
+using AuctionService.Data;
+using AuctionService.Entities;
+using Contracts;
+using MassTransit;
+
+namespace AuctionService.Consumers;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext _context;
+
+    public AuctionFinishedConsumer(AuctionDbContext context)
+    {
+        _context = context;
+
+    }
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--- Auction Finished Consumer Consuming ---");
+
+        var auction = await _context.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice ? 
+            Status.Finished : Status.ReserveNotMet;
+        
+        await _context.SaveChangesAsync();
+    }
+}
